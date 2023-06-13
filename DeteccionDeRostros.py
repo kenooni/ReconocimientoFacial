c7d7a7872b0603edca55b96a442ac0de207e56f5
kenooni <78682335+kenooni@users.noreply.github.com>
2023-06-13 11:43:04 -0600

    DeteccionDeRostros.py

--- DeteccionDeRostros.py
+++ DeteccionDeRostros.py
@@ -1,21 +1,16 @@
 import cv2
 import os
 
-dataPath = 'C:/Users/toyzr/OneDrive/Documents/SchoolStuff/Semestre8/AprendizajeProfundo/RedesNeuronales/DatosEntrenamiento/Data'
+dataPath = 'C:/Users/toyzr/OneDrive/Documents/SchoolStuff/Semestre8/AprendizajeProfundo/RedesNeuronales/DatosEntrenamiento/Data' #Usamos el mismo path que en los archivos previos
 imagePaths = os.listdir(dataPath)
 print('imagePaths=',imagePaths)
 
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Leyendo el modelo
-#face_recognizer.read('modeloEigenFace.xml')
-#face_recognizer.read('modeloFisherFace.xml')
 face_recognizer.read('modeloLBPHFace.xml')
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture('kevin.mp4')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
@@ -33,24 +28,6 @@ while True:
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		'''
-		# EigenFaces
-		if result[1] < 5700:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
-		'''
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
 		# LBPHFace
 		if result[1] < 70:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
